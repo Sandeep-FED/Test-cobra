1d498a41a8bd7ffe137fd0e5d8a21de37fc537c6
Sandeep P S <sandymankara11@gmail.com>
2023-03-26 18:00:50 +0530

    src: clean up and basic template bringup
    Signed-off-by: Sandeep P S <sandymankara11@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import Button from "@material-ui/core/Button"
+import { useState } from "react"
+import { Panel, PanelType } from "@fluentui/react/lib/Panel"
+import { useBoolean } from "@fluentui/react-hooks"
+import TextField from "@material-ui/core/TextField"
 
 function App() {
+  // TODO: useboolean hook for controlling the state of the panel
+  const [isOpen, { setTrue: openPanel, setFalse: dismissPanel }] =
+    useBoolean(false)
+
+  // TODO: For storing the input data in states for later sending purpose
+  const [age, setAge] = useState("")
+  const [segmentName, setSegmentName] = useState("")
+
+  const Addsections = () => {
+    console.log("new section added")
+  }
+
+  const handleInputChange = (event, value) => {
+    setSegmentName(event.target.value)
+  }
+  console.log(segmentName)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <section>
+        <Button
+          variant='outlined'
+          color='primary'
+          onClick={openPanel}
+          className='btn'
         >
-          Learn React
-        </a>
-      </header>
+          Save segment
+        </Button>
+        <Panel
+          headerText='Saving Segment'
+          isOpen={isOpen}
+          type={PanelType.medium}
+          onDismiss={dismissPanel}
+          closeButtonAriaLabel='Close'
+        >
+          <div className='container'>
+            <TextField
+              label='Name of the segment'
+              placeholder='Enter the name of the segment'
+              variant='outlined'
+              fullWidth={true}
+              onChange={handleInputChange}
+            />
+            <p>
+              To save your segment, you need to add the schemas to build the
+              query
+            </p>
+            <p onClick={Addsections}>Add new schema</p>
+          </div>
+        </Panel>
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
